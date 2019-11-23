586a87365cdb92254e0a70802ea0cb474970bbd9
Romchik <romakabanec997@gmail.com>
2019-11-23 16:43:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,17 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "flex";
     
 }
+window.addEventListener("DOMContentLoaded", function() {
+    var url = document.querySelector("#tel"),
+      val = str = url.defaultValue,
+      flag;
+    url.value = val;
+    url.addEventListener("input", function(event) {
+      if (url.selectionStart > val.length - 1 && !flag) str = url.value, str = str.slice(val.length), str = val + str;
+      flag = false;
+      url.value = str
+    });
+    url.addEventListener("paste", function(event) {
+      if (url.selectionStart < val.length) flag = true
+    })
+  });
